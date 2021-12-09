53bd18dac7c56616770218340b12b99e8dff22c7
vijudan <danielgithub365@gmail.com>
2021-12-09 13:03:44 -0600

    extended core

--- core/views.py
+++ core/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+html_base = """
+<h1>My personal web</h1>
+<ul>
+    <li><a href="/">Cover page</a></li>
+    <li><a href="/about-me/">About</a></li>
+</ul>
+"""
+
 def home(request):
-    return HttpResponse("<h1>My personal web</h1><h2>Cover page</h2>")
+    return HttpResponse(f"{html_base}<h2>Cover page</h2><p>This is the cover</p>")
+
+def about(request):
+    return HttpResponse(f"{html_base} <h2>About</h2><p>My name is Daniel and I am a programmer</p>")
+
 
