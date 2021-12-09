a57180f9a78729362a82d42e86b2336a85aefb0e
vijudan <danielgithub365@gmail.com>
2021-12-09 13:15:18 -0600

    adding portfolio and contact to core

--- core/views.py
+++ core/views.py
@@ -7,6 +7,8 @@ html_base = """
 <ul>
     <li><a href="/">Cover page</a></li>
     <li><a href="/about-me/">About</a></li>
+    <li><a href="/portfolio/">Portfolio</a></li>
+    <li><a href="/contact/">Contact</a></li>
 </ul>
 """
 
@@ -17,3 +19,8 @@ def about(request):
     return HttpResponse(f"{html_base} <h2>About</h2><p>My name is Daniel and I am a programmer</p>")
 
 
+def portfolio(request):
+    return HttpResponse(f"{html_base}<h2>Portfolio</h2><p>Here are the projects</p>")
+
+def contact(request):
+    return HttpResponse(f"{html_base}<h2>Contact</h2><p>Here you can contact me</p>")
