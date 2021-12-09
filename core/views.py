a28376c2c3bc5d46dd42f50d1cf7bf471ca46c27
vijudan <danielgithub365@gmail.com>
2021-12-09 16:48:04 -0600

    test templates

--- core/views.py
+++ core/views.py
@@ -2,25 +2,15 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-html_base = """
-<h1>My personal web</h1>
-<ul>
-    <li><a href="/">Cover page</a></li>
-    <li><a href="/about-me/">About</a></li>
-    <li><a href="/portfolio/">Portfolio</a></li>
-    <li><a href="/contact/">Contact</a></li>
-</ul>
-"""
-
 def home(request):
-    return HttpResponse(f"{html_base}<h2>Cover page</h2><p>This is the cover</p>")
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(f"{html_base} <h2>About</h2><p>My name is Daniel and I am a programmer</p>")
+    return render(request, "core/about.html")
 
 
 def portfolio(request):
-    return HttpResponse(f"{html_base}<h2>Portfolio</h2><p>Here are the projects</p>")
+    return render(request, "core/portfolio.html")
 
 def contact(request):
-    return HttpResponse(f"{html_base}<h2>Contact</h2><p>Here you can contact me</p>")
+    return render(request, "core/contact.html")
