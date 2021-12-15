a7556ba7ab1bbd212feba9d17370ad735f450541
vijudan <danielgithub365@gmail.com>
2021-12-15 13:22:59 -0600

    Admin panel improvements

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.db import models
 from .models import Project
-# Register your models here.
 
-admin.site.register(Project)
+# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(Project, ProjectAdmin)
 
--- portfolio/models.py
+++ portfolio/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200) 
-    description = models.TextField() 
-    image = models.ImageField()
-    created = models.DateTimeField(auto_now_add=True) #Se creará la fecha automáticamente  
-    updated = models.DateTimeField(auto_now=True) #Se actualiza cada vez que se ejecuta una instancia 
+    title = models.CharField(max_length=200, verbose_name='Título') 
+    description = models.TextField(verbose_name='Descripción') 
+    image = models.ImageField(verbose_name='Imagen', upload_to='projects')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación') #Se creará la fecha automáticamente  
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de edición') #Se actualiza cada vez que se ejecuta una instancia 
 
+    class Meta:
+        verbose_name = 'proyecto'
+        verbose_name_plural = 'proyectos'
+
+    def __str__(self):
+        return self.title 
+
+    
